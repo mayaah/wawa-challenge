ea86eb0a4802c8fa6d37233b58ba91dcd59d7da3
Maya Angelica Hernandez <mayaah@berkeley.edu>
2024-05-23 12:42:26 +0100

    Add User model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:username) {"username"}
+
+  subject { 
+    described_class.new(
+      username: username, 
+      email: "test@example.com",
+      password: "password"
+    )  
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is invalid without a username"  do
+    subject.username = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without an email"  do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without a password"  do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  context 'when username is not unique' do
+    before { 
+      described_class.create!(
+        username: username, 
+        email: 'email@email.com', 
+        password: 'password'
+      )
+    }
+    it {expect(subject).to be_invalid}
+  end
+end
